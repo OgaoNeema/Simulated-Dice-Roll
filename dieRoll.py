d31b38b6b6688ab037bab4e650e277a1386c0a60
OgaoNeema <neemaogao@gmail.com>
2025-03-07 23:02:32 +0300

    A few updates

--- dieRoll.py
+++ dieRoll.py
@@ -1,13 +1,15 @@
-import random
+import random #For generating random numbers
 
 #Define the random number generated
-random_numbers = [random.random() for _ in range(1000)]
+random_numbers = [random.random() for _ in range(1000)] #A list of the 100 random numbers
 
 #Frequency of occurrence of a face
-frequency = {i: 0 for i in range(1, 7)}
+frequency = {i: 0 for i in range(1, 7)} #for the 6 faces
+total = 0
+percentage_total = 0
 
 for number in random_numbers:
-    if 0 < number < 1/6 :
+    if 0 < number < 1/6 : #if the no lies between 0 and 1/6, increase the frequency of face 1(index 0) by 1
         frequency[1] += 1
     elif 1/6 < number < 2/6:
         frequency[2] += 1
@@ -20,16 +22,14 @@ for number in random_numbers:
     else:
         frequency[6] += 1
 
-total = 0
-percentage_total = 0
-
 #Results
 print(f"{'Face':<5}{'Frequency':<10}{'Percentage'}")
 print("-" * 25)
 for face, freq in frequency.items():
-    percentage = (freq / 1000) * 100
+    percentage = (freq / 10) # instead of (freq / 1000) * 100
     print(f"{face:<5}   {freq:<10}{percentage:.1f}%")
     percentage_total += percentage
     total += freq
 
-print(f"{'Total:'} {total}      {percentage_total:.1f}%")
+print("-" * 25)
+print(f"{'Total:':<5}   {total:<10}{percentage_total:.1f}%")
